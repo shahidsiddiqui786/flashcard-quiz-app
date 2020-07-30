b5b70c58f9a101f50b3ae361709d9a9521db33d2
shahidsiddiqui786 <56132385+shahidsiddiqui786@users.noreply.github.com>
2020-07-30 14:53:17 +0530

    Updates

---
+++ precache-manifest.41e1a93cf0c8a6619316a91325962975.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f4246f28b01cf0d97f5ef0ccc32f1e7",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "02ac4d5c969949ad941c",
+    "url": "/my-app/static/css/main.685d0e95.chunk.css"
+  },
+  {
+    "revision": "9cdf3db0763be495a5eb",
+    "url": "/my-app/static/js/2.d3d812e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.d3d812e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02ac4d5c969949ad941c",
+    "url": "/my-app/static/js/main.0580b94c.chunk.js"
+  },
+  {
+    "revision": "1e07441035ecaab89538",
+    "url": "/my-app/static/js/runtime-main.97105902.js"
+  }
+]);
